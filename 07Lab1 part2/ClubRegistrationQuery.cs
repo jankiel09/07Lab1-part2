981cce33ef26664fdd2955b4a5662a4c295284eb
jankiel09 <ezekielambat@gmail.com>
2025-11-19 20:21:26 +0800

    Edited the ClubRegistrationClass Added the constructor and initialized an object. Added a method naemd DisplayList that contains Added RegisterStudent method.

--- 07Lab1 part2/ClubRegistrationQuery.cs
+++ 07Lab1 part2/ClubRegistrationQuery.cs
@@ -18,11 +18,65 @@ namespace _07Lab1_part2
 
         public DataTable dataTable;
         public BindingSource bindingSource;
-
+        public string _FirstName, _MiddleName, _LastName, _Gender, _Program;
         private string connectionString = "Data Source=LAB-A-PC00;Initial Catalog=PaguiliganClubDB;Persist " +
             "Security Info=True;User ID=paguiligan.j;Password=***********;Trust Server Certificate=True";
 
+        ClubRegistrationQuery ()
+        {
+            sqlConnect = new SqlConnection(connectionString);
+            dataTable = new DataTable();
+            bindingSource = new BindingSource();
+        }
 
-        public string _FirstName, _MiddleName, _LastName, _Gender, _Program;
+        public Boolean DisplayList()
+        {
+            string ViewClubMembers = "SELECT S* FROM ClubRegistrations";
+            dataTable.Clear();
+            sqlAdapter = new SqlDataAdapter(ViewClubMembers, sqlConnect);
+            sqlAdapter.Fill(dataTable);
+            bindingSource.DataSource = dataTable;
+
+            return true;
+        }
+
+        public bool RegisterStudent(int ID, long StudentID, string FirstName, string
+                                    MiddleName, string LastName, int Age, string Gender, string Program)
+        {
+            sqlCommand = new SqlCommand("INSERT INTO ClubMembers VALUES(@ID, @StudentID, @FirstName, @MiddleName, @LastName, @Age, @Gender, @Program)", sqlConnect);
+            sqlCommand.Parameters.Add("@ID", SqlDbType.Int).Value = ID;
+            sqlCommand.Parameters.Add("@RegistrationID", SqlDbType.BigInt).Value = StudentID;
+            sqlCommand.Parameters.Add("@StudentID", SqlDbType.VarChar).Value = StudentID;
+            sqlCommand.Parameters.Add("@FirstName", SqlDbType.VarChar).Value = FirstName;
+            sqlCommand.Parameters.Add("@MiddleName", SqlDbType.VarChar).Value = MiddleName;
+            sqlCommand.Parameters.Add("@LastName", SqlDbType.VarChar).Value = LastName;
+            sqlCommand.Parameters.Add("@Age", SqlDbType.Int).Value = Age;
+            sqlCommand.Parameters.Add("@Gender", SqlDbType.VarChar).Value = Gender;
+            sqlCommand.Parameters.Add("@Program", SqlDbType.VarChar).Value = Program;
+            sqlConnect.Open();
+            sqlCommand.ExecuteNonQuery();
+            sqlConnect.Close();
+            return true;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
