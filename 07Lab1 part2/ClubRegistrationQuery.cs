c14a9d3350ff4952255a2bbcfe01b5968cb3ba1b
jankiel09 <ezekielambat@gmail.com>
2025-11-19 19:48:50 +0800

    Add project files.

---
+++ 07Lab1 part2/ClubRegistrationQuery.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Windows.Forms;
+
+namespace _07Lab1_part2
+{
+    public  class ClubRegistrationQuery
+    {
+        private SqlConnection sqlConnect;
+        private SqlCommand sqlCommand;
+        private SqlDataAdapter sqlAdapter;
+        private SqlDataAdapter sqlReader;
+
+        public DataTable dataTable;
+        public BindingSource bindingSource;
+
+        private string connectionString = "Data Source=LAB-A-PC00;Initial Catalog=PaguiliganClubDB;Persist " +
+            "Security Info=True;User ID=paguiligan.j;Password=***********;Trust Server Certificate=True";
+
+
+        public string _FirstName, _MiddleName, _LastName, _Gender, _Program;
+    }
+}
